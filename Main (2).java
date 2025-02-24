c4e9296590824e1c439cb04f70a136b9d159e39c
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-02-24 19:34:34 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome number.");
+        } else {
+            System.out.println(num + " is not a palindrome number.");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+        
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        
+        return originalNum == reversedNum;
+    }
+}
